9953bd98152a070ad1be4f2423850773339bfd4f
Ubuntu <ubuntu@ip-172-31-17-204.eu-central-1.compute.internal>
2021-07-04 20:37:58 +0000

    bugfix

--- ub-tracker.py
+++ ub-tracker.py
@@ -5,7 +5,7 @@ import requests
 
 def save_to_file(date, n, filename="./data.csv"):
     with open(filename, 'a') as f:
-        f.write(f"{date};{n}\n")
+        f.write(f"{date},{n}\n")
     
 def get_free_seats():
     url = "https://checkin.ub.uni-freiburg.de/ajax/external/checkBack.php"
@@ -18,12 +18,12 @@ def get_free_seats():
         print(free, time.time()-start)
         request.close()
         t = time.localtime()
-        date = f"{t[2]}.{t[1]}.{t[0]}.{(t[3]+2)%24}.{t[4]}.{t[5]}"  # Date in the format DD.MM.YYYY.Hour.Minute.Seconds
-        if free != last:  # Save to file if number of free seats changes
+        date = f"{t[2]}.{t[1]}.{t[0]}.{(t[3]+2)%24}.{t[4]}.{t[5]}"  # Date in the format DD.MM.YYYY.Hour.Minute.Second
+        if free != last: 
             last = free
-            save_to_file(date, free, "./data2.csv")
+            save_to_file(date, free, "./data.csv")
         time.sleep(1)
-        if (int(t[3])+2)%24 == 23:  # Wait until UB opens again
+        if (int(t[3])+2)%24 == 0:  # Wait until UB opens again
             time.sleep(8*60*60)
 
 
