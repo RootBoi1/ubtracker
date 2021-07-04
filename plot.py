cacade022731ef0daf3a8f03ce813c2724234da5
Ubuntu <ubuntu@ip-172-31-17-204.eu-central-1.compute.internal>
2021-07-04 23:55:25 +0000

    add sleep

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import datetime as d
 
 def plot_data(path="./data.csv"):
     with open(path, 'r') as f:
-        data = f.readlines)
+        data = f.readlines()
     x = []
     y = []
     for line in data:
