6553b31217542ae2ddcd8a8be3cd7f9e586ef70a
Ubuntu <ubuntu@ip-172-31-17-204.eu-central-1.compute.internal>
2021-07-04 13:39:33 +0000

    timesave fix

--- ub-tracker.py
+++ ub-tracker.py
@@ -1,3 +1,4 @@
+import gc
 import time
 from bs4 import BeautifulSoup as bfs
 from requests_html import HTMLSession
@@ -12,15 +13,14 @@ def save_to_file(date, n, filename="./data.csv"):
 def get_free_seats():
     session = HTMLSession()
     URL = "https://www.ub.uni-freiburg.de/freie-plaetze/#:~:text=Aktuell%20noch%20416%20freie%20Pl%C3%A4tze%20im%20Lesesaal."
-
-    r = session.get(URL, stream=True)
     last_iteration = 0
     while True:
+        r = session.get(URL, stream=True)
         start = time.time()
-        r.html.render(retries=1)
+        r.html.render(timeout=10)
         soup = bfs(r.html.html, "lxml")
         t = time.localtime()
-        date = f"{t[2]}.{t[1]}.{t[3]}.{t[4]}.{t[5]}"
+        date = f"{t[2]}.{t[1]}.{t[0]}.{t[3]+2}.{t[4]}.{t[5]}"
         string = str(soup.find(id="ds")).split(">")
         free = string[3].split(" ")[0]
         try:
@@ -31,7 +31,10 @@ def get_free_seats():
             last_iteration = free
             save_to_file(date, free)
         print(free, time.time()-start)
+        r.close()
         time.sleep(1)
+        if int(date[3]+2) == 23:
+            time.sleep(8*60*60)
 
 
 if __name__=="__main__":
