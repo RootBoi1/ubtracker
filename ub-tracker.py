23e29a9a5e489817fe97603e6fdc15ec52fc1a7e
Ubuntu <ubuntu@ip-172-31-17-204.eu-central-1.compute.internal>
2021-07-04 16:12:07 +0000

    Better code

--- ub-tracker.py
+++ ub-tracker.py
@@ -1,39 +1,29 @@
-import gc
 import time
-from bs4 import BeautifulSoup as bfs
-from requests_html import HTMLSession
-from requests_html import AsyncHTMLSession as asyncs
+import json
+import requests
 
 
 def save_to_file(date, n, filename="./data.csv"):
     with open(filename, 'a') as f:
         f.write(f"{date};{n}\n")
-        
     
 def get_free_seats():
-    session = HTMLSession()
-    URL = "https://www.ub.uni-freiburg.de/freie-plaetze/#:~:text=Aktuell%20noch%20416%20freie%20Pl%C3%A4tze%20im%20Lesesaal."
-    last_iteration = 0
+    url = "https://checkin.ub.uni-freiburg.de/ajax/external/checkBack.php"
+    last = 0
     while True:
-        r = session.get(URL, stream=True)
         start = time.time()
-        r.html.render(timeout=10)
-        soup = bfs(r.html.html, "lxml")
-        t = time.localtime()
-        date = f"{t[2]}.{t[1]}.{t[0]}.{t[3]+2}.{t[4]}.{t[5]}"
-        string = str(soup.find(id="ds")).split(">")
-        free = string[3].split(" ")[0]
-        try:
-            free = int(free)
-        except:
-            free = 0
-        if free != last_iteration:
-            last_iteration = free
-            save_to_file(date, free)
+        request = requests.post(url)
+        data = json.loads(request.text)
+        free = data["cap"] - data["count"]
         print(free, time.time()-start)
-        r.close()
+        request.close()
+        t = time.localtime()
+        date = f"{t[2]}.{t[1]}.{t[0]}.{(t[3]+2)%24}.{t[4]}.{t[5]}"  # Date in the format DD.MM.YYYY.Hour.Minute.Seconds
+        if free != last:  # Save to file if number of free seats changes
+            last = free
+            save_to_file(date, free, "./data2.csv")
         time.sleep(1)
-        if int(date[3]+2) == 23:
+        if (int(t[3])+2)%24 == 23:  # Wait until UB opens again
             time.sleep(8*60*60)
 
 
