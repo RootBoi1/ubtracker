73b20d60d898dbb70a039b9748b942930d207394
Johannes Mannhardt <johannesmannhardt@web.de>
2021-07-05 13:42:33 +0200

    added simple flask app and html, css, js

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, send_from_directory
+
+# set the project root directory as the static folder
+app = Flask(__name__, template_folder='.')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    print(path)
+    return send_from_directory('css', path)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def get_data():
+    # return available seats data
+    return [1, 2, 3, 4, 5]
+
+if __name__ == "__main__":
+    app.run()
